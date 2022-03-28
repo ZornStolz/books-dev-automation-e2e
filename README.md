5ccf21364aea1995b7f259589ac8636d0c3e3b4a
ZornStolz <1143875103@u.icesi.edu.co>
2022-03-27 22:42:15 -0500

    Read tests implemented

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # books-dev-automation-e2e
+
+## **Overview of the testing**
+
+### *Create service*
+
+- 
+
+### *Read service*
+
+- Test if the petition that the front is doing to the back gives a response
+- Test that there is data loaded from the petition
+
+### *Update service*
+
+- 
+
+### *Delete service*
+
+- 
---
+++ cypress/integration/CrudTesting/ReadService.spec.js
@@ -0,0 +1,22 @@
+const uri = 'localhost:4200/dashboard';
+
+describe ('Check if the  aplication is loaded properly', () => {
+    before(() => {
+        //Arrange
+        cy.visit(uri)
+    })
+    
+    it('The GET petition to load the Overview returns a 200', () => {
+        
+        //Act
+        let back = 'http://localhost:8080/books'
+        cy.request('GET', back).then ((res) => {
+            //Assert
+            expect(res.status).to.equal(200)
+        })
+    })
+
+    it('The data was loaded', () => {
+        cy.get('.ant-table-cell')
+    })
+})
